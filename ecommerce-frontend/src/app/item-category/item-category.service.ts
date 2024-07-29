e85ce298907eb59ad770c2c453f3da913d2ba6a0
Darshil Patel <darshilpatel11001@gmail.com>
2024-07-29 15:53:25 -0400

    Created the frontend part that shows categories and products from the backend

---
+++ ecommerce-frontend/src/app/item-category/item-category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { Category } from '../category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemCategoryService {
+  private apiUrl = 'http://localhost:8080/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
